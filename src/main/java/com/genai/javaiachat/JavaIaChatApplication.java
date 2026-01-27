a0999cb6e88a1c8f06323939cf7ec918154684fa
Nathan LYONNET <nathan.lyonnet@et.esiea.fr>
2026-01-27 15:25:55 +0100

    initial commit structure MVC

---
+++ src/main/java/com/genai/javaiachat/JavaIaChatApplication.java
@@ -0,0 +1,13 @@
+package com.genai.javaiachat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaIaChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaIaChatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/genai/javaiachat/JavaIaChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.genai.javaiachat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaIaChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
